c330347caa460060623b5d7eb970880a2482de7f
Arseniy Zhivyh <zhivyhars@yandex.ru>
2020-06-15 20:07:05 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace TestIPCamera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(642, 122);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(82, 26);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(642, 199);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(82, 27);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(39, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(491, 352);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(591, 298);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 458);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using System.Xml;
+using System.IO;
+using System.Net;
+
+namespace TestIPCamera
+{
+    public partial class Form1 : Form
+    {
+        MJPEGStream stream;
+        string[] Inners = new string[5];
+        Dictionary<string, string> IDs = new Dictionary<string, string>(5);
+        int i = 0;
+        int j = 0;
+        public XmlDocument MakeRequest()
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://demo.macroscop.com:8080/configex?login=root");
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                XmlDocument doc = new XmlDocument();
+                //doc.Load(new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(1251)));
+                doc.Load(new StreamReader(response.GetResponseStream()));
+                XmlElement xRoot = doc.DocumentElement;
+                foreach (XmlNode xnode in xRoot)
+                {
+                    try
+                    {
+                        Inners[i] = xnode["Configuration"]["Channels"]["ChannelInfo"].InnerText;
+                        i++;
+                    }
+                    catch(NullReferenceException e)
+                    {
+
+                    }
+                }
+                foreach (string inner in Inners)
+                {
+                    string[] words = inner.Split(new char[] { '"' });
+                    IDs.Add(words[1], words[3]);
+                }
+                comboBox1.DataSource = new BindingSource(IDs, null);
+                comboBox1.DisplayMember = "Key";
+                comboBox1.ValueMember = "Value";
+                return doc;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            MakeRequest();
+        }
+        void stream_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = bmp;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            stream = new MJPEGStream("http://demo.macroscop.com:8080/mobile?login=root&channelid=773bad89-c18a-4e7e-a70d-c2a37897a92d");
+            stream.NewFrame += stream_NewFrame;
+            stream.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            stream.Stop();
+        }
+    }
+}
